17bb36c21d590a1019fc43647942debe4fd2dc5c
DangNguyen <67234142+dangnguyen1004@users.noreply.github.com>
2025-09-25 22:07:47 +0700

    first commit

---
+++ src/Baitap1.java
@@ -0,0 +1,16 @@
+public class Baitap1 {
+    public static void main(String[] args) {
+        /*
+         * Homework #1 - Primitives
+         *
+         * As a tradition, programmers write a "Hello, World!" application when starting a new project.
+         * Let's do your first app!
+         *
+         * Assignment #1
+         * 1.) Print the message: "Hello, World!"
+         */
+
+        // Write your code below this line
+        System.out.println("Hello world!");
+    }
+}
